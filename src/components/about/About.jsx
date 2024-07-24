30a114e72b4b478df50544b311557ec3054b0423
Eba1623 <ebisagutema19@gmail.com>
2024-07-24 12:24:02 +0300

    test commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
            
         </div>
         <div className='about-right'>
-          <h3>Lorem ipsum dolor sit.</h3>
+          <h3>Lorem ipsm dolor sit.</h3>
           <h1>Lorem, ipsum.</h1>
           <p>Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dolorem non cupiditate repudiandae laudantium quidem laboriosam eveniet et atque impedit. dolor sit amet consectetur adipisicing elit. Ratione reprehenderit consequuntur adipisci! Sed recusandae, accusantium dolorem corporis hic culpa qui libero quidem perspiciatis! Reiciendis, sapiente vitae quasi odio corrupti aut? Corrupti.</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, exercitationem veritatis tenetur incidunt accusamus harum quidem facilis dicta alorem10
